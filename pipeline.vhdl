239e73c926d49a573c89c09546fffdaf97142513
abcustodio1 <abcustodio1@up.edu.ph>
2018-05-12 15:14:29 +0800

    Fixed documnetation

--- pipeline.vhdl
+++ pipeline.vhdl
@@ -12,15 +12,25 @@ architecture file_reading of pipeline is
 			process is
 				file in_file : text open read_mode is "file_name.txt";
 				variable in_line : line;
-				variable opcode: std_logic_vector(14 downto 0);
+				variable opcode: std_logic_vector(20 downto 0);
+				variable operation: std_logic_vector(2 downto 0);
+				variable mode: std_logic;
+				variable operand: std_logic_vector(4 downto 0);
  			begin
  				while not endfile(in_file) loop
  					readline(in_file, in_line);
- 					test_loop: for count in 0 to 14 loop
+ 					test_loop: for count in 0 to 20 loop
  	  					read(in_line, opcode(count));
- 	  					report std_logic'image(opcode(count));
  	  					wait for 1 ns;
  					end loop;
+
+				    operation(0) := opcode(0);
+				    operation(1) := opcode(1);
+				    operation(2) := opcode(2);
+ 					
+ 					report "operation: " & 	std_logic'image(operation(0)) &
+ 											std_logic'image(operation(1)) &
+ 											std_logic'image(operation(2));
 
  				end loop;
  				assert false report "simulation done" severity note;
